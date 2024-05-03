6cb0beb88b66b486d53629b690017e741dbfe337
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2024-05-03 23:31:43 +0200

    Bug fix 🧪

--- Scripts/confettis.js
+++ Scripts/confettis.js
@@ -10,10 +10,15 @@ generatePackNameButton.addEventListener('click', function() {
     });
 });
 
+
 function showConfettiAuthor() {
-var input = document.getElementById('auteur');
-    party.confetti(input, {
-        count: party.variation.range(150, 80),
-        shapes: ["square", "circle", "roundedRectangle"]
-    });
+    // Vérifier si un nom d'utilisateur est enregistré dans le stockage local
+    var savedUsername = localStorage.getItem("username");
+    if (savedUsername) {
+        var input = document.getElementById('auteur');
+        party.confetti(input, {
+            count: party.variation.range(150, 80),
+            shapes: ["square", "circle", "roundedRectangle"]
+        });
+    }
 }
